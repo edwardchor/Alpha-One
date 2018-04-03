b662e9f8213a72031c2240c84b20359d721b6c99
edwardchor <czy8023@me.com>
2018-04-03 02:01:25 -0400

    routine

--- sbatch_script.sh
+++ sbatch_script.sh
@@ -4,14 +4,14 @@
 #SBATCH --ntasks-per-node=4
 ##SBATCH --cpus-per-task=8
 
-##SBATCH --time=10:00:00
+#SBATCH --time=10:00:00
    
 #SBATCH --mem=52GB
-##SBATCH --gres=gpu:k80:2
+#SBATCH --gres=gpu
 #SBATCH --job-name=AlphaOne1S1T
  
 #SBATCH --mail-type=END
 #SBATCH --mail-user=zc1213@nyu.edu
    
 #SBATCH --output=slurm.out
-python GoMain.py 
+python -u GoMain.py 
