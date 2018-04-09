f021a1ce949b7a79a3437ea35e279eb7498a816d
edwardchor <czy8023@me.com>
2018-04-08 21:10:19 -0400

    routine

--- sbatch_script.sh
+++ sbatch_script.sh
@@ -3,9 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
 ##SBATCH --cpus-per-task=8
-
-#SBATCH --time=24:00:00
-   
+#SBATCH --time=48:00:00 
 #SBATCH --mem=52GB
 #SBATCH --gres=gpu
 #SBATCH --job-name=AlphaOne1S1T
